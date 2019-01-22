92319d9bd366513e31b036bcb667b6bd3f265bed
blakereeves19 <blake.reeves19@gmail.com>
2019-01-21 21:12:40 -0600

    Completed lab

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,46 +2,46 @@ import React, { Component } from 'react';
 import { Jumbotron, Form, FormGroup, Label, Input, Button } from 'reactstrap';
 import Timeline from './Timeline';
 
-let data = [
-    {
-        chirp: "Hello React!"
-    },
-    {
-        chirp: "Chirper is amazing!"
-    },
-    {
-        chirp: "I love pizza!"
-    }
-]
-
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            chirp: "",
-            chirps: []
+            chirps: [],
+            username: "",
+            chirp: ""
         };
     }
 
     componentDidMount() {
-        this.setState({ chirps: data});
+        let chirps = [
+            {
+                username: 'Blake',
+                chirp: "Hello React!"
+            },
+            {
+                username: 'Robin',
+                chirp: "Chirper is amazing!"
+            },
+            {
+                username: 'Kyle',
+                chirp: "I love pizza!"
+            }
+        ];
+        this.setState({ chirps });
     }
 
     handleSubmitChirp(e) {
         e.preventDefault();
         console.log(this.state.username);
+        console.log(this.state.chirp);
 
         let chirps = this.state.chirps;
-        let chirp = {
-            chirp: this.state.chirp
-        };
+        let newChirp = { chirp: this.state.chirp, username: this.state.username };
+        chirps.unshift(newChirp);
 
-        chirps.unshift(chirp);
-
-        this.setState({ 
+        this.setState({
             username: '',
             chirp: '',
-            chirps
         });
     }
 
@@ -55,35 +55,46 @@ class App extends Component {
                         </Jumbotron>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-6 m-5">
-                        <Form className="border border-primary rounded">
-                            <FormGroup className="form-control-lg">
-                                <Label>Username</Label>
-                                <Input 
-                                    type="text"
-                                    placeholder="Username"
-                                    value={this.state.username}
-                                    onChange={(e) => this.setState({ username: e.target.value})}
-                                />
-                                <Label>Chirp</Label>
-                                <Input
-                                    type="text"
-                                    placeholder="What's happening?"
-                                    value={this.state.chirp}
-                                    onChange={(e) => this.setState({ chirp: e.target.value })}
-                                />
-                            </FormGroup>
-                            <Button
-                                type="submit"
-                                className="mt-5"
-                                size="md"
-                                color="primary"
-                                onClick={ (e) => this.handleSubmitChirp(e)}
-                            >Chirp it!</Button>
-                        </Form>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-11 m-5">
+                            <Form className="border border-primary rounded">
+                                <FormGroup className="form-control-lg">
+                                    <Label>Username</Label>
+                                    <Input
+                                        type="text"
+                                        placeholder="Username"
+                                        value={this.state.username}
+                                        onChange={(e) => this.setState({ username: e.target.value })}
+                                    />
+                                </FormGroup>
+                                <FormGroup className="form-control-lg mt-5">
+                                    <Label>Chirp</Label>
+                                    <Input
+                                        className=""
+                                        type="text"
+                                        placeholder="What's happening?"
+                                        value={this.state.chirp}
+                                        onChange={(e) => this.setState({ chirp: e.target.value })}
+                                    />
+                                </FormGroup>
+                                <Button
+                                    type="submit"
+                                    className="mt-5 ml-3 mb-3"
+                                    size="md"
+                                    color="primary"
+                                    onClick={(e) => this.handleSubmitChirp(e)}
+                                >Chirp it!</Button>
+                            </Form>
+                        </div>
                     </div>
-                    <Timeline chirps={this.state.chirps}/>
+                </div>
+                <div className="containder">
+                    <div className="row">
+                        {this.state.chirps.map((chirp, index) => {
+                            return <Timeline key={index} chirp={chirp} />
+                        })}
+                    </div>
                 </div>
             </>
         );
--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
+import { Card, CardTitle, CardText, Col} from 'reactstrap';
 
 const Timeline = (props) => {
     return (
-        <div className="row">
-            {props.chirps.map((chirp, index) => {
-                return (
-                    <div key={index} className="col-md-12 m-2">
-                        <div className="card ml-5 mt-3 p-2 border-primary">
-                            <h1 className="p-2 border border-dark border-top-0 border-left-0 border-right-0">Chirp</h1>
-                            <p className="p-2">{chirp.chirp}</p>
-                        </div>
-                    </div>
-                );
-            })}
-        </div>
+        <Col sm="6">
+            <Card body className="col-md-12 m-2">
+                <CardTitle className="p-2 border border-dark border-top-0 border-left-0 border-right-0">{props.chirp.username} Chirped!</CardTitle>
+                <CardText className="p-2">{props.chirp.chirp}</CardText>
+            </Card>
+        </Col>
     );
 };
 
